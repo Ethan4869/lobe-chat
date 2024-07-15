81553b5527cbf741bef1cdb4f5c992a959d36d51
李振宇 <zhenyuelfli@gmail.com>
2024-07-15 16:40:33 +0800

    vercel中只选日本节点，配置文件中添加所有节点排除hk

--- next.config.mjs
+++ next.config.mjs
@@ -142,6 +142,29 @@ const nextConfig = {
       },
     ];
   },
+
+ 
+runtime: 'edge',
+regions: [
+  'arn1',
+  'bom1',
+  'cdg1',
+  'cle1',
+  'cpt1',
+  'dub1',
+  'fra1',
+  'gru1',
+  'hnd1',
+  'iad1',
+  'icn1',
+  'kix1',
+  'lhr1',
+  'pdx1',
+  'sfo1',
+  'sin1',
+  'syd1',
+ ],
+  
 };
 
 const noWrapper = (config) => config;
